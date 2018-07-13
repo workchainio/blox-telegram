feffcb6324ac6cc528bc10fbf586123321d8736e
ipetrovic11 <ivan@workchain.io>
2018-07-13 11:38:04 +0200

    Fixing target middleware

--- middlewares/telegraf-target.js
+++ middlewares/telegraf-target.js
@@ -4,7 +4,8 @@ exports.TargetMiddlware = async (ctx, next) => {
 
     if(ctx.update.message.reply_to_message){
         
-        let member = await Member.find(ctx.update.message.reply_to_message.from.id);
+        const data = ctx.update.message.reply_to_message.from;
+        let member = await Member.find(data.id);
         if(!member){
             let admins = await ctx.getChatAdministrators();
             member = await Member.create(data, admins);
